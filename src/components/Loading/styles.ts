04f585be1b2598946c81fc357d879fe56e7c65bf
Raphael Torres <rtcarva@gmail.com>
2024-12-11 23:30:09 -0300

    projeto todo list

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet,Dimensions} from "react-native";
+import { themas } from "../../global/themes";
+
+
+export const style = StyleSheet.create({
+    container:{
+        position:'absolute',
+        width:'100%',
+        height:'100%',
+        zIndex:9999,
+        backgroundColor:themas.Colors.blackTransparent,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
